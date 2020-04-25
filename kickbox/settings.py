b8f45d080792cdeeb1d993bd64616225e380ac2a
Yaroslav Kobel <yarokobel@gmail.com>
2020-04-26 01:42:22 +0300

    Change Langwich

--- kickbox/settings.py
+++ kickbox/settings.py
@@ -98,7 +98,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'ru-RU'
+LANGUAGE_CODE = 'ua-UA'
 
 TIME_ZONE = 'UTC'
 
